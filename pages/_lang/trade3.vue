b3d019c9bc6af5bfa9abe156363da83d74a5c45e
chengjiang <15712859926@163.com>
2019-03-02 18:18:43 +0800

    添加交易挂出

---
+++ pages/_lang/trade3.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>我是交易挂出</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="" scoped>
+</style>
