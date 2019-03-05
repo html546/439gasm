4d1057de5cd31d17ec6d7aafa1bbdc91af100f3f
chengjiang <15712859926@163.com>
2019-03-05 14:08:10 +0800

    推薦好友

--- pages/_lang/friend.vue
+++ pages/_lang/friend.vue
@@ -1,19 +1,224 @@
 <template>
   <div>
-    <p>推荐好友</p>
+    <div class="ad"></div>
+    <div class="friend">
+      <el-card class="box-card">
+        <div slot="header">
+          <p style="text-align:left;">{{$t('friend.register')}}</p>
+        </div>
+        <div>
+          <el-form
+            ref="form"
+            label-width="140px"
+            style="width:560px"
+            @submit.native="onSubmit"
+          >
+            <el-form-item :label="$t('friend.member')">
+              <el-input
+                name="username"
+                v-model="username"
+                :readonly="true"
+              ></el-input>
+            </el-form-item>
+            <el-form-item
+              v-for="(val,key) in formContent"
+              :key="key"
+              :label="val.name"
+              v-show="val.input!=='hidden'"
+            >
+              <el-input
+                v-model="val.default"
+                :name="key"
+                v-if="val.input=='text'"
+              ></el-input>
+              <el-select
+                :name="key"
+                v-else-if="val.input=='select'&&key=='sex'"
+                v-model="sex"
+                style="width:100%;"
+              >
+                <el-option
+                  v-for="(val1,key1) in val.select"
+                  :key="key1"
+                  :label="val1"
+                  :value="key1"
+                >
+                </el-option>
+              </el-select>
+              <el-select
+                :name="key"
+                v-else-if="val.input=='select'&&key=='memberrank'"
+                v-model="memberrank"
+                style="width:100%;"
+              >
+                <el-option
+                  v-for="(val1,key1) in val.select"
+                  :key="key1"
+                  :label="val1"
+                  :value="key1"
+                >
+                </el-option>
+              </el-select>
+              <el-select
+                :name="key"
+                v-else-if="val.input=='select'&&key=='bank_name'"
+                v-model="bank_name"
+                style="width:100%;"
+              >
+                <el-option
+                  v-for="(item,index) in bank_names"
+                  :key="index"
+                  :label="item.bank_names"
+                  :value="item.id"
+                >
+                </el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item :label="$t('friend.pass1')">
+              <el-input
+                name="pass1"
+                v-model="pass1"
+              ></el-input>
+            </el-form-item>
+            <el-form-item :label="$t('friend.pass1c')">
+              <el-input
+                name="pass1c"
+                v-model="pass1c"
+              ></el-input>
+            </el-form-item>
+            <el-form-item :label="$t('friend.pass2')">
+              <el-input
+                name="pass2"
+                v-model="pass2"
+              ></el-input>
+            </el-form-item>
+            <el-form-item :label="$t('friend.pass2c')">
+              <el-input
+                name="pass2c"
+                v-model="pass2c"
+              ></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                native-type="submit"
+              >{{$t('friend.submit')}}</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: '',
   data() {
     return {
-
+      formContent: '',
+      bank_names: '',
+      pass1: '',
+      pass1c: '',
+      pass2: '',
+      pass2c: '',
+      province: '',
+      city: '',
+      area: '',
+      provinces: [],
+      citys: [],
+      areas: [],
+      bank_name: '',
+      sex: '',
+      memberrank: '',
+      username: '',
     }
-  }
+  },
+  created() {
+    this.getPage();
+    this.getBanks();
+  },
+  methods: {
+    getPage() {
+      axios.post('/api/member/register', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        type: 1
+      }).then(res => {
+        console.log(res);
+        this.username = res.data.data.defaultname;
+        this.formContent = res.data.data.regdatasets;
+        for (const key in this.formContent) {
+          if (key == 'sex' && this.formContent[key].default !== '') {
+            this.sex = this.formContent[key].default;
+          } else if (key == 'memberrank' && this.formContent[key].default !== '') {
+            this.memberrank = this.formContent[key].default;
+          } else if (key == 'bank_name' && this.formContent[key].default !== '') {
+            this.bank_name = this.formContent[key].dafault;
+          }
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    getBanks() {
+      axios.post('/api/member/getbanks', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid
+      }).then(res => {
+        this.bank_names = res.data.data;
+      }).catch(err => {
+        console.log(err);;
+      })
+    },
+    onSubmit(event) {
+      console.log(event);
+      event.preventDefault();
+      var formdata = new FormData();
+      for (let i = 0; i < event.target.length - 4; i++) {
+        if (event.target[i].name == 'bank_name' || event.target[i].name == 'memberrank' || event.target[i].name == 'sex') {
+          continue;
+        }
+        formdata.append(event.target[i].name, event.target[i].value);
+      }
+      formdata.append('username', this.username);
+      formdata.append('userid', this.$store.state.message.userid);
+      formdata.append('sessionid', this.$store.state.message.sessionid);
+      formdata.append('bank_name', this.bank_name);
+      formdata.append('sex', this.sex);
+      formdata.append('memberrank', this.memberrank);
+      axios.post('/api/member/registersave', formdata).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            message: res.data.msg,
+            type: 'success',
+            showClose: true
+          })
+        } else if (res.data.status == 0) {
+          this.$message({
+            message: res.data.msg,
+            type: "error",
+            showClose: true
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    }
+  },
 }
 </script>
 
-<style lang="" scoped>
+<style >
+.ad {
+  width: 100%;
+  background: url("~assets/ad.png") no-repeat center center;
+  background-size: cover;
+  height: 180px;
+}
+.friend {
+  width: 1200px;
+  margin: 55px auto 100px;
+}
 </style>
--- pages/_lang/trade2.vue
+++ pages/_lang/trade2.vue
@@ -11,6 +11,7 @@
         element-loading-text="拼命加载中"
         element-loading-spinner="el-icon-loading"
         element-loading-background="rgba(0, 0, 0, 0.8)"
+        class="trade2_btn"
       >
 
         <el-table-column
@@ -84,7 +85,54 @@
           :label="$t('trade2.operate')"
         >
           <template slot-scope="scope">
-            <div></div>
+            <template v-if="CheckState1(scope)">
+              <el-button
+                type="warning"
+                size="mini"
+                @click="handleRemit(scope)"
+              >
+                {{$t('trade2.remit')}}
+              </el-button>
+              <el-button
+                type="danger"
+                size="mini"
+                @click="handleCancel(scope)"
+              >
+                {{$t('trade2.cancel')}}
+              </el-button>
+            </template>
+            <template v-else-if="CheckState2(scope)">
+              <el-button
+                type="warning"
+                size="mini"
+                @click="handleArbitrate(scope)"
+              >
+                {{$t('trade2.arbitration')}}
+              </el-button>
+              <el-button
+                type="danger"
+                size="mini"
+                @click="handleSure(scope)"
+              >
+                {{$t('trade2.confirm')}}
+              </el-button>
+            </template>
+            <template v-else-if="checkState3(scope)">
+              <el-button
+                type="primary"
+                size="mini"
+                @click="handleUpload(scope)"
+              >
+                {{$t('trade2.proof')}}
+              </el-button>
+            </template>
+            <el-button
+              type="primary"
+              size="mini"
+              @click="handleCheck(scope)"
+            >
+              {{$t('trade2.details')}}
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -99,6 +147,179 @@
         :current-page="currentPage"
       >
       </el-pagination>
+      <el-dialog
+        title="交易详情"
+        :visible.sync="dialogFormVisible"
+        top="5vh"
+      >
+        <el-form :model="form">
+          <el-form-item label="卖家编号">
+            <el-input
+              v-model="form.saleusername"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="开户行">
+            <el-input
+              v-model="form.bank_name"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="银行卡号">
+            <el-input
+              v-model="form.bank_number"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="开户名">
+            <el-input
+              v-model="form.bank_username"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="买家编号">
+            <el-input
+              v-model="form.username"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="购买数量">
+            <el-input
+              v-model="form.num"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="购买价格">
+            <el-input
+              v-model="form.oneprice"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="购买时间">
+            <el-input
+              v-model="buytime"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+      <el-dialog
+        title="交易汇款"
+        :visible.sync="dialogFormVisible1"
+        :close="handleClose"
+        top="5vh"
+      >
+        <el-form
+          :model="form1"
+          label-width="80px"
+        >
+          <el-form-item label="卖家编号">
+            <el-input
+              v-model="form1.saleusername"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="汇款金额">
+            <el-input
+              v-model="money"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item
+            label="汇款时间"
+            class="time"
+          >
+            <el-date-picker
+              v-model="remitTime"
+              type="datetime"
+              placeholder="选择日期时间"
+              align="right"
+              :picker-options="pickerOptions1"
+            >
+            </el-date-picker>
+          </el-form-item>
+          <el-form-item label="汇款凭证">
+            <el-upload
+              class="upload-demo"
+              :action="upload"
+              :limit="1"
+              :on-success="handleSuccess"
+              :data="uploadData"
+              :on-exceed="handleExceed"
+            >
+              <el-button
+                size="small"
+                type="primary"
+              >点击上传</el-button>
+              <img
+                :src="imageUrl"
+                style="max-width:200px;"
+                alt=""
+              >
+            </el-upload>
+          </el-form-item>
+          <el-form-item style="text-align:left;">
+            <el-button
+              type="primary"
+              @click="handleRemitSubmit"
+            >立即提交</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+      <el-dialog
+        title="申诉内容"
+        :visible.sync="dialogFormVisible2"
+        top="5vh"
+      >
+        <el-form>
+          <el-form-item label="申诉内容">
+            <el-input
+              type="textarea"
+              v-model="content"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              @click="handleArbitrateSubmit"
+            >立即提交</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
+      <el-dialog
+        title="上传交易凭据"
+        :visible.sync="dialogFormVisible3"
+        top="5vh"
+      >
+        <el-form>
+          <el-form-item label="交易凭据">
+            <el-upload
+              class="upload-demo"
+              :action="upload"
+              :limit="1"
+              :on-success="handleSuccess"
+              :data="uploadData"
+              :on-exceed="handleExceed"
+            >
+              <el-button
+                size="small"
+                type="primary"
+              >点击上传</el-button>
+              <img
+                :src="imageUrl"
+                style="max-width:200px;"
+                alt=""
+              >
+            </el-upload>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              @click="handleTradeSubmit"
+            >立即提交</el-button>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -113,7 +334,48 @@ export default {
       detailData: [],
       total: 0,
       currentPage: 1,
-      loading: true
+      loading: true,
+      dialogFormVisible: false,
+      dialogFormVisible1: false,
+      dialogFormVisible2: false,
+      dialogFormVisible3: false,
+      form: {},
+      form1: {},
+      buytime: '',
+      remitTime: '',
+      upload: '/api/trade/upload',
+      uploadData: {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+      },
+      imageUrl: '',
+      content: '',
+      pickerOptions1: {
+        shortcuts: [
+          {
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          },
+          {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          },
+          {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }
+        ]
+      },
     }
   },
   created() {
@@ -123,6 +385,11 @@ export default {
       this.getPage(page);
     } else {
       this.getPage(1);
+    }
+  },
+  computed: {
+    money() {
+      return this.form1.num * this.form1.oneprice;
     }
   },
   methods: {
@@ -155,8 +422,200 @@ export default {
       this.getPage(val);
       this.$store.state.commit('SET_PAGE3', val);
     },
-    t(...params) {
-      return this.$t.apply(this, params)
+    CheckState1(scope) {
+      if (scope.row.state == 0 && scope.row.username == this.$store.state.message.username) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    CheckState2(scope) {
+      if (scope.row.state == 1 && scope.row.saleusername == this.$store.state.message.username) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    checkState3(scope) {
+      if (scope.row.state == 4 && scope.row.username == this.$store.state.message.username) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    handleCheck(scope) {
+      this.dialogFormVisible = true;
+      console.log(scope);
+      this.form = scope.row;
+      this.buytime = format1(scope.row.buytime * 1000);
+      this.id = scope.row.id;
+    },
+    handleRemit(scope) {
+      console.log(scope);
+      this.form1 = scope.row;
+      this.dialogFormVisible1 = true;
+      this.id = scope.row.id;
+    },
+    handleArbitrate(scope) {
+      this.dialogFormVisible2 = true;
+      this.id = scope.row.id;
+    },
+    handleUpload(scope) {
+      this.id = scope.row.id;
+      this.dialogFormVisible3 = true;
+    },
+    handleSuccess(res, file) {
+      console.log(file);
+      this.imageUrl = file.response.data.url;
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+    },
+    handleRemitSubmit() {
+      axios.post('/api/trade/remitbuytrade', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        id: this.id,
+        remittime: this.remitTime,
+        image_input: this.imageUrl
+      }).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            message: res.data.msg,
+            type: 'success',
+            showClose: true,
+            onClose: this.onclose1()
+          })
+        } else {
+          this.$message({
+            type: "error",
+            message: res.data.msg,
+            showClose: true
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    handleCancel(scope) {
+      axios.post('/api/trade/cancelbuytrade', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        id: scope.row.id
+      }).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            message: res.data.msg,
+            type: 'success',
+            showClose: true,
+            onClose: this.onclose()
+          })
+        } else {
+          this.$message({
+            message: res.data.msg,
+            type: 'error',
+            showClose: true,
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    handleArbitrateSubmit() {
+      axios.post('/api/trade/stopbuytrade', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        id: this.id,
+        saledata: this.content
+      }).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            type: "success",
+            message: res.data.msg,
+            showClose: true,
+            onClose: this.onclose2()
+          })
+        } else {
+          this.$message({
+            type: "error",
+            message: res.data.msg,
+            showClose: true,
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    handleTradeSubmit() {
+      axios.post('/api/trade/proofbuytrade', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        id: this.id,
+        buydata: this.imageUrl
+      }).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            type: "success",
+            message: res.data.msg,
+            showClose: true,
+            onClose: this.onclose4()
+          })
+        } else {
+          this.$message({
+            type: "error",
+            message: res.data.msg,
+            showClose: true,
+          })
+        }
+      })
+    },
+    handleSure(scope) {
+      axios.post('/api/trade/okbuytrade', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        id: scope.row.id
+      }).then(res => {
+        if (res.data.status == 1) {
+          this.$message({
+            type: "success",
+            message: res.data.msg,
+            showClose: true,
+            onClose: this.onclose3()
+          })
+        } else {
+          this.$message({
+            type: "error",
+            message: res.data.msg,
+            showClose: true,
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    onclose() {
+      this.getPage(this.currentpage);
+    },
+    onclose1() {
+      this.dialogFormVisible1 = false;
+      this.getPage(this.currentpage);
+      this.image_input = '';
+      this.remitTime = '';
+    },
+    onclose2() {
+      this.dialogFormVisible2 = false;
+      this.getPage(this.currentpage);
+    },
+    onclose3() {
+      this.getPage(this.currentpage);
+    },
+    onclose4() {
+      this.dialogFormVisible3 = false;
+      this.getPage(this.currentpage);
+    },
+    handleClose() {
+      this.image_input = '';
+      this.remitTime = '';
     }
   },
   filters: {
@@ -194,5 +653,18 @@ body {
 }
 .pagination {
   margin-top: 20px;
+}
+.trade2_btn button {
+  width: 100px;
+}
+.trade2_btn .el-button + .el-button {
+  margin-left: 0;
+}
+.time .el-date-editor.el-input,
+.time .el-date-editor.el-input__inner {
+  width: 100%;
+}
+.upload-demo {
+  text-align: left;
 }
 </style>
