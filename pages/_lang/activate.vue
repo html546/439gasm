549f057b44783fa7209be6e2893cf9929e456724
chengjiang <15712859926@163.com>
2019-03-06 17:30:20 +0800

    会员激活

--- pages/_lang/activate.vue
+++ pages/_lang/activate.vue
@@ -1,19 +1,125 @@
 <template>
   <div>
-    <p>我是會員激活頁面</p>
+    <div class="activate">
+      <div class="form_box">
+        <el-form label-width="140">
+          <el-form-item label="激活账户余额">
+            <el-input
+              v-model="balance"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="当前usdt价格">
+            <el-input
+              v-model="price"
+              :readonly="true"
+            ></el-input>
+          </el-form-item>
+          <p>激活账号需要激活账户{{level}}(参数),请确认余额充足.</p>
+          <el-form-item label="扣款方式选择">
+            <el-select
+              v-model="mode"
+              style="width:100%;"
+              @change="handleChange"
+            >
+              <el-option
+                label="全额注册"
+                value="1"
+              ></el-option>
+              <el-option
+                label="组合方式注册"
+                value="2"
+              ></el-option>
+            </el-select>
+          </el-form-item>
+          <el-button
+            type="primary"
+            @click="handleSubmit"
+          >立即提交</el-button>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: '',
   data() {
     return {
-
+      balance: '',
+      price: '',
+      level: '',
+      mode: '',
+      memberInfo: ''
     }
-  }
+  },
+  created() {
+    this.getPage();
+  },
+  methods: {
+    getPage() {
+      axios.post('/api/member/meconfirm', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid
+      }).then(res => {
+        console.log(res);
+        this.balance = res.data.data.financeinfo[0].money;
+        this.price = res.data.data.price;
+        this.memberInfo = res.data.data.memberInfo;
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    handleChange(val) {
+      if (val == 1) {
+        this.level = this.memberInfo.jh1;
+      } else if (val == 2) {
+        this.level = this.memberInfo.jh2;
+      }
+    },
+    handleSubmit() {
+      axios.post('/api/member/meconfirmSave', {
+        userid: this.$store.state.message.userid,
+        sessionid: this.$store.state.message.sessionid,
+        regtype: this.mode
+      }).then(res => {
+        console.log(res);
+        if (res.data.status == 1) {
+          this.$message({
+            type: "success",
+            message: res.data.msg,
+            showClose: true,
+            onClose: this.onclose()
+          })
+        } else {
+          this.$message({
+            type: "error",
+            message: res.data.msg,
+            showClose: true,
+            onClose: this.onclose()
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    },
+    onclose() {
+      this.mode = '';
+      this.level = '';
+    }
+  },
 }
 </script>
 
 <style>
+.activate {
+  width: 1200px;
+  margin: 50px auto;
+}
+.form_box {
+  width: 600px;
+  margin: 0 auto;
+}
 </style>
